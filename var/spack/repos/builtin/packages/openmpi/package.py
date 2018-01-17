91945d90b9cef318341e884f61225a42c06db9c0
Levi Baber <baber@iastate.edu>
2018-01-17 16:42:15 -0600

    libfabrics: cleanup openmpi unnecessary libdir config arg

--- var/spack/repos/builtin/packages/openmpi/package.py
+++ var/spack/repos/builtin/packages/openmpi/package.py
@@ -217,6 +217,7 @@ class Openmpi(AutotoolsPackage):
     conflicts('fabrics=pmi', when='@:1.5.4')  # PMI support was added in 1.5.5
     conflicts('fabrics=mxm', when='@:1.5.3')  # MXM support was added in 1.5.4
     conflicts('fabrics=libfabric', when='@:1.8') #libfabric support was added in 1.10.0
+    #It may be worth considering making libfabric an exclusive fabrics choice
 
     def url_for_version(self, version):
         url = "http://www.open-mpi.org/software/ompi/v{0}/downloads/openmpi-{1}.tar.bz2"
@@ -306,9 +307,6 @@ class Openmpi(AutotoolsPackage):
         # Fabrics and schedulers
         config_args.extend(self.with_or_without('fabrics'))
         config_args.extend(self.with_or_without('schedulers'))
-
-        if 'libfabric' in self.fabrics:
-            config_args.append('--with-libfabric-libdir={0}'.format(join_path(spec['libfabric'].prefix, 'lib')))
 
         # Hwloc support
         if spec.satisfies('@1.5.2:'):
