13f5f284f5cff4817b5cd62eaa32ff73f28b274e
lingnanyuan <1297162327@qq.com>
2018-03-22 13:24:21 -0500

    perl-devel-overloadinfo: Update version.

--- var/spack/repos/builtin/packages/perl-devel-overloadinfo/package.py
+++ var/spack/repos/builtin/packages/perl-devel-overloadinfo/package.py
@@ -29,8 +29,8 @@ class PerlDevelOverloadinfo(PerlPackage):
     """Returns information about overloaded operators for a given class"""
 
     homepage = "http://search.cpan.org/~ilmari/Devel-OverloadInfo-0.004/lib/Devel/OverloadInfo.pm"
-    url      = "http://search.cpan.org/CPAN/authors/id/I/IL/ILMARI/Devel-OverloadInfo-0.004.tar.gz"
+    url      = "http://search.cpan.org/CPAN/authors/id/I/IL/ILMARI/Devel-OverloadInfo-0.005.tar.gz"
 
-    version('0.004', '97a27e31858b073daba54121d57be705')
+    version('0.005', '607b65dfe9fdb47df780f3b22dcb7917')
 
     depends_on('perl-mro-compat', type=('build', 'run'))
