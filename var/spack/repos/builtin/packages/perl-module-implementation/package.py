cd46f88ac3a2a6c257fe9f9eddf593d8e27e8164
lingnanyuan <1297162327@qq.com>
2018-03-22 13:09:09 -0500

    perl-module-implementation: Add new package.

--- var/spack/repos/builtin/packages/perl-module-implementation/package.py
+++ var/spack/repos/builtin/packages/perl-module-implementation/package.py
@@ -26,15 +26,10 @@ from spack import *
 
 
 class PerlModuleImplementation(PerlPackage):
-    """Loads one of several alternate underlying implementations for a
-    module"""
+    """Loads one of several alternate underlying implementations for
+    a module."""
 
-    homepage = "http://search.cpan.org/~drolsky/Module-Implementation/lib/Module/Implementation.pm"
+    homepage = "http://search.cpan.org/~drolsky/Module-Implementation-0.09/lib/Module/Implementation.pm"
     url      = "http://search.cpan.org/CPAN/authors/id/D/DR/DROLSKY/Module-Implementation-0.09.tar.gz"
 
     version('0.09', '52e3fe0ca6b1eff0488d59b7aacc0667')
-
-    depends_on('perl-module-runtime', type=('build', 'run'))
-    depends_on('perl-test-fatal', type=('build', 'run'))
-    depends_on('perl-test-requires', type=('build', 'run'))
-    depends_on('perl-try-tiny', type=('build', 'run'))
