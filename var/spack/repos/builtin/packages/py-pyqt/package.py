40f4c22e2e108ef55c77111c12477446ce8d243b
Levi Baber <baber@iastate.edu>
2018-03-27 15:26:25 -0500

    py-pyqt: adding version 5

--- var/spack/repos/builtin/packages/py-pyqt/package.py
+++ var/spack/repos/builtin/packages/py-pyqt/package.py
@@ -33,13 +33,16 @@ class PyPyqt(Package):
     url      = "http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.3/PyQt-x11-gpl-4.11.3.tar.gz"
 
     version('4.11.3', '997c3e443165a89a559e0d96b061bf70')
+    version('5.10.1', '9932e971e825ece4ea08f84ad95017837fa8f3f29c6b0496985fa1093661e9ef', 
+            url = 'https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.10.1/PyQt5_gpl-5.10.1.tar.gz')
 
     extends('python')
     depends_on('py-sip', type=('build', 'run'))
 
     # TODO: allow qt5 when conditional deps are supported.
     # TODO: Fix version matching so that @4 works like @:4
-    depends_on('qt@:4+phonon+dbus')
+    depends_on('qt@:4+phonon+dbus', when='@:4')
+    depends_on('qt@:5', when='@5:')
 
     def install(self, spec, prefix):
         python('configure.py',
