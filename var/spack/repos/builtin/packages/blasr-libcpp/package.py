3485a3884c22baf08e366f943bb15d0779566a88
mpperry <mpperry@mallishious.las.iastate.edu>
2018-05-31 09:38:02 -0500

    added link to github issue for hdf5 version

--- var/spack/repos/builtin/packages/blasr-libcpp/package.py
+++ var/spack/repos/builtin/packages/blasr-libcpp/package.py
@@ -37,7 +37,10 @@ class BlasrLibcpp(Package):
 
     depends_on('pbbam')
     depends_on('hdf5+cxx@1.8.12:1.8.99')
-# maximum version is 1.8.20 currently. There doesn't appear to be a major version 1.9 and the 1.10.1 version doesn't build correctly. 
+# maximum version is 1.8.20 currently. There doesn't appear to be a
+# major version 1.9 and the 1.10.1 version doesn't build correctly.
+# https://github.com/PacificBiosciences/blasr/issues/355
+
     depends_on('pkgconfig')
     depends_on('python')
 
